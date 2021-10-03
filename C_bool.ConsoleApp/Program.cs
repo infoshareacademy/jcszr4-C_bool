44d5220e1efd600131ef2912bcd951f6db387906
Natalia Kulik <n.zielinska@mtl.com.pl>
2021-10-03 21:13:19 +0200

    C_bool

---
+++ C_bool.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_bool.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
