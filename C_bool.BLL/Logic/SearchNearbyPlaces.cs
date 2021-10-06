fe34020f594db202634d363fd3df78c2ff2829b5
Maciej Walas <walasmaciej94@gmail.com>
2021-10-06 22:05:30 +0200

    Added search for nearby places by radius

---
+++ C_bool.BLL/Logic/SearchNearbyPlaces.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using C_bool.BLL.Places;
+
+namespace C_bool.BLL.Logic
+{
+    public static class SearchNearbyPlaces
+    {
+        const double pi = Math.PI;
+        const double earthRadius = 6371000;
+
+        public static double DistanceBetweenPlaces(double place1Latitude, double place1Longitude, double place2Latitude, double place2Longitude)
+        {
+            double sin_place1Latitude = Math.Sin(Radians(place1Latitude));
+            double sin_place2Latitude = Math.Sin(Radians(place2Latitude));
+            double cos_place1Latitude = Math.Cos(Radians(place1Latitude));
+            double cos_place2Latitude = Math.Cos(Radians(place2Latitude));
+
+            double distance = earthRadius * Math.Acos(sin_place1Latitude * sin_place2Latitude + cos_place1Latitude * cos_place2Latitude * Math.Cos(Radians(place1Longitude) - Radians(place2Longitude)));
+
+            return distance;
+        }
+
+        public static double Radians(double x)
+        {
+            return x * pi / 180;
+        }
+
+        public static List<Place> GetPlaces(double lat1, double lon1, double radius, List<Place> places)
+        {
+            List<Place> outputList = new List<Place>();
+
+            foreach (var place in places)
+            {
+                var distance = DistanceBetweenPlaces(lat1, lon1, place.Geometry.Location.Latitude,
+                    place.Geometry.Location.Longitude);
+                if (distance <= radius)
+                {
+                    outputList.Add(place);
+                }
+            }
+            return outputList;
+        }
+    }
+}
--- C_bool.ConsoleApp/Program.cs
+++ C_bool.ConsoleApp/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using C_bool.BLL.Places;
 using C_bool.BLL.Repositories;
 using C_bool.BLL.Users;
+using C_bool.BLL.Logic;
 
 namespace C_bool.ConsoleApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             var placesRepository = new PlacesRepository();
@@ -16,10 +18,17 @@ namespace C_bool.ConsoleApp
             var usersRepository = new UsersRepository();
             usersRepository.AddFileDataToRepository();
 
-            User.PrintInformation(usersRepository.Users, "");
+           // User.PrintInformation(usersRepository.Users, "");
 
-            Place.PrintInformation(placesRepository.Places, "");
+           // Place.PrintInformation(placesRepository.Places, "");
+
+            double radius = 200;
+
+            List<Place> nearbyPlaces = SearchNearbyPlaces.GetPlaces(50.26855256464339, 19.02468223856004, radius, placesRepository.Places);
+            Console.WriteLine("MIEJSCA W POBLIŻU:");
+            Place.PrintInformation(nearbyPlaces, "");
 
         }
+        
     }
 }
