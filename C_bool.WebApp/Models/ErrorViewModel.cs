21dd396b7345475157a25172230d37addf0ebb00
Natalia Kulik <n.zielinska@mtl.com.pl>
2021-11-07 20:16:40 +0100

    share WebApp

---
+++ C_bool.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C_bool.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
