f9a2adb9cdbe4397b8b871c2994c7d75c7fbd923
Natalia Kulik <n.zielinska@mtl.com.pl>
2022-01-18 21:26:06 +0100

    Model GameTask and Profile

---
+++ C_bool.WebApp/Models/Profiles/GameTaskProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using C_bool.BLL.Models.GameTask;
+
+namespace C_bool.WebApp.Models.Profiles
+{
+    public class GameTaskProfile :Profile
+    {
+        public GameTaskProfile()
+        {
+            CreateMap<BLL.DAL.Entities.GameTask, GameTaskViewModel>()
+                .ForMember(dest => dest.PlaceId, o => o.MapFrom(src => src.Place.Id))
+                .ForMember(dest => dest.Name, o => o.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ShortDescription, o => o.MapFrom(src => src.ShortDescription))
+                .ForMember(dest => dest.Description, o => o.NullSubstitute("?"))
+                .ForMember(dest => dest.Photo, o => o.Ignore())
+                .ForMember(dest => dest.Type, o => o.MapFrom(src => src.Type))
+                .ForMember(dest => dest.TextCriterion, o => o.NullSubstitute("?"));
+        }
+    }
+}
