426943eb38ba11cca14ebe0cd9f0156da5b663d6
Natalia Kulik <n.zielinska@mtl.com.pl>
2022-01-18 21:06:29 +0100

    GameTask_Service

---
+++ C_bool.BLL/Logic/TaskTypeTranslated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C_bool.BLL.Enums;
+
+namespace C_bool.BLL.Logic
+{
+    class TaskTypeTranslated
+    {
+        public static Dictionary<TaskType, string> EnumTaskTypeTranslated { get; set; } = new()
+        {
+            { TaskType.FirstComeFirstServed, "Kto pierwszy ten lepszy" },
+            { TaskType.TakeAPhoto, "Zrób zdjęcie" },
+            { TaskType.CheckInToALocation, "Zamelduj się w wybranym miejscu" },
+            { TaskType.CheckInAtTheSpecifiedTime, "Zameldowanie o określonej godzinie" },
+            { TaskType.TextEntry, "Wprowadź tekst" }
+        };
+    }
+}
