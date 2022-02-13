d201fef18cb7a5432cddc02c43d2a92293daf4f4
Natalia Kulik <n.zielinska@mtl.com.pl>
2022-02-13 18:58:47 +0100

    ReportingModule_API_Mailing

---
+++ C_bool.ASP.NET.API_Mailing/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace C_bool.ASP.NET.API_Mailing
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
